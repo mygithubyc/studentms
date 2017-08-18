5507227365bdb477489a9378d236806f944d9f9a
ygd <helloworld8881@sina.com>
2017-08-18 11:30:42 +0800

    bye

--- studentms Maven Webapp/src/main/java/com/kingsoft/studentms/service/BasicService.java
+++
@@ -1,7 +0,0 @@
-package com.kingsoft.studentms.service;
-
-import org.springframework.web.multipart.MultipartFile;
-
-public interface BasicService {
-	public String uploadMutiFile(MultipartFile file,String path);
-}
